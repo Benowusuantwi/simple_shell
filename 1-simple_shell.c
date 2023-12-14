812d90aae8d3e232c8cb4a45a193e6ecde16755e
Ben_X <benowusuantwi819@gmail.com>
2023-12-14 06:36:07 -0800

    All done

--- 1-simple_shell.c
+++ 1-simple_shell.c
@@ -1,4 +1,3 @@
-#include "main.h"
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
@@ -7,6 +6,9 @@
 #include <sys/stat.h>
 #include <string.h>
 #include <fcntl.h>
+#include "main.h"
+
+void process_command(char *command);
 
 /**
  * main - a program that mimics the functons of a Unix shell
@@ -22,13 +24,89 @@ int main(void)
 		printf("$ ");
 		if (fgets(command, sizeof(command), stdin) == NULL)
 		{
-			printf("\n");
-			break;
+			if (feof(stdin))
+			{
+				printf("\n");
+				break;
+			}
+			else
+			{
+				perror("Error reading command");
+				exit(EXIT_FAILURE);
+			}
 		}
 
 		command[strcspn(command, "\n")] = '\0';
 
+		if (*command == '\0')
+		{
+			continue;
+		}
+
 		process_command(command);
 	}
 	return (0);
+}
+
+void executecommand(char *args[]);
+/**
+ * process_command - a function that processes commands typed...
+ * by the user
+ * @command: input
+ * Return: non
+ */
+
+void process_command(char *command)
+{
+	char *args[MAX_ARGS], **environment = environ;
+	int arg_count = 0;
+
+	args[arg_count] = strtok(command, "");
+	while (args[arg_count] != NULL && arg_count < MAX_ARGS - 1)
+	{
+		arg_count++;
+		args[arg_count] = strtok(NULL, " ");
+	}
+	args[arg_count] = NULL;
+	if (strcmp(args[0], "exit") == 0)
+	{
+		printf("\n");
+		exit(EXIT_SUCCESS);
+	}
+	else if (strcmp(args[0], "env") == 0)
+	{
+		while (*environment != NULL)
+		{
+			printf("%s\n", *environment);
+			environment++;
+		}
+		return;
+	}
+	executecommand(args);
+}
+/**
+ * executecommand - a function that executes input commands...
+ * from the user
+ * @args: input arguments from the user
+ * Return: non
+ */
+void executecommand(char *args[])
+{
+	pid_t pid = fork();
+
+	if (pid == -1)
+	{
+		perror("Error forking");
+		exit(EXIT_FAILURE);
+	}
+	if (pid == 0)
+	{
+		execvp(args[0], args);
+		perror(args[0]);
+		exit(EXIT_FAILURE);
+	}
+	else
+	{
+		waitpid(pid, NULL, 0);
+	}
 }
